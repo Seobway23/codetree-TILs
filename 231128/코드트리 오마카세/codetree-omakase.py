817ff4125664fca3e25772aa74ae3a9a38de4e54
Seobway23 <122852052+Seobway23@users.noreply.github.com>
2023-11-28 09:48:18 +0900

    Title: 코드트리 오마카세, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231128/코드트리 오마카세/codetree-omakase.py
@@ -0,0 +1,98 @@
+from copy import deepcopy
+
+L, Q = map(int, input().split())
+# 필요 자료 구조
+sushi_lst = dict() # 초밥 리스트
+people_dict = dict() # 사람 cnt
+people_index = dict() # 사람 자리
+
+def circle(time):
+    global L, sushi_lst
+    temp = deepcopy(sushi_lst)
+
+    # table 순회
+    for i in sushi_lst:
+
+        flag = 0
+        # 타임에 따라서 다르게 하기
+        t, loc, name = sushi_lst[i][0], sushi_lst[i][1], i
+        t, loc, time = int(t), int(loc), int(time)
+        dif = time - t
+        
+        # 범위 내 있는지 확인
+        people_list = people_dict.keys()
+
+        if name in people_list:
+            s, e = loc, ((loc + dif) % L)
+
+            # 있는 지 확인
+            if dif >= L:
+                people_dict[name] -= 1
+                del temp[i]
+
+
+            elif e < s:
+                if s < people_index[name]  + L < e + L:
+                    people_dict[name] -= 1
+                    del temp[i]
+                    
+            else: 
+                if s <= people_index[name] <= e:
+                    people_dict[name] -= 1
+                    del temp[i]
+
+            # cnt가 0인지 확인
+            if people_dict[name] == 0:
+                del people_dict[name]
+                del people_index[name]
+                flag= 1
+        # 갱신
+        if flag == 0 and i in temp:
+            # if i in temp:
+            # print("i:",i, ", temp[i][0]:", temp[i][0] )
+            temp[i][0] = time
+            temp[i][1] = ((loc + dif) % L)
+
+    # 스시 딕셔너리 갱신
+    sushi_lst = temp
+
+    # print("갱신된 suhsi:", sushi_lst)
+    # print("현재 people:",people_dict)
+    return
+
+
+
+def plus_sushi(lst):
+    time, loc, name = lst[1:]
+    time, loc = int(time), int(loc)
+    sushi_lst[name] = [time, loc]
+    circle(time)
+    return
+
+def plus_people(lst):
+    time, loc, name, cnt = lst[1:]
+    time, loc, cnt = int(time), int(loc), int(cnt)
+    people_dict[name] = cnt
+    people_index[name] = loc
+    circle(time)
+    return
+
+def print_sushi(lst):
+    time = lst[1]
+    circle(time)
+    print(len(people_dict), len(sushi_lst))
+    return
+
+
+
+for i in range(Q):
+    lst = list(input().split())
+    if lst[0] == "100" :
+        plus_sushi(lst)
+
+
+    if lst[0] == "200":
+        plus_people(lst)
+
+    if lst[0] == "300":
+        print_sushi(lst)
