0f1183133655f06c00d93cfc9bf84e0a4c5b1594
Seobway23 <122852052+Seobway23@users.noreply.github.com>
2023-12-11 14:29:45 +0900

    Title: 진법 변환, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231211/진법 변환/base-conversion.py
+++ 231211/진법 변환/base-conversion.py
@@ -1,15 +1,19 @@
 n, m = input().split()
+m = int(m)
 
 # string이 나오면 숫자로 바꾸기
-if m.isdigit():
-    m = int(m)
-else:
-    m = ord(m) - 55
+def func(m):
+    if m.isdigit():
+        m = int(m)
+    else:
+        m = ord(m) - 55
+    
+    return m
 
 #메인 로직
 N, sum, up = len(n), 0, 0
 for i in range(N-1, -1, -1):
-    bit = int(n[i])
+    bit = func(n[i])
     sum += bit * ( m ** up) 
     up += 1
     # print(sum, up, bit)
